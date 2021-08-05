294fe2421b60f54bc8a104c798505337623e4098
Ata-92 <atakal92@gmail.com>
2021-08-05 16:14:41 +0300

    Add model to admin page

--- home/models.py
+++ home/models.py
@@ -7,3 +7,6 @@ class Contact(models.Model):
     phone_number = models.CharField(max_length=50)
     email = models.EmailField(max_length=254)
     message = models.TextField()
+
+    def __str__(self):
+        return self.name
