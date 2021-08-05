a00d9a67dd70035e1687f8c7736626f592c1889a
Ata-92 <atakal92@gmail.com>
2021-08-05 17:02:54 +0300

    Add media folder

--- home/admin.py
+++ home/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from home.models import Contact
+from home.models import Contact, Teacher
 
 # Register your models here.
 
 class ContactAdmin(admin.ModelAdmin):
     list_display = ["name", "phone_number", "email", "message"]
 
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ["name", "phone", "email", "speciality"]
+
 admin.site.register(Contact, ContactAdmin)
+admin.site.register(Teacher, TeacherAdmin)
