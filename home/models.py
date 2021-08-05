2ac6b33eab08db45c532c3afed2f63ef3725077c
Ata-92 <atakal92@gmail.com>
2021-08-05 16:46:15 +0300

    Add Teacher model

--- home/models.py
+++ home/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms.models import ModelChoiceIterator
 
 # Create your models here.
 
@@ -7,6 +8,23 @@ class Contact(models.Model):
     phone_number = models.CharField(max_length=50)
     email = models.EmailField(max_length=254)
     message = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField(upload_to="teacher", default="default.png")
+    email = models.EmailField(max_length=254)
+    phone = models.CharField(max_length=50)
+
+    TITLE = (
+        ("english", "English Teacher"),
+        ("scratch", "Scratch Teacher"),
+        ("science", "Science Teacher"),
+        ("sport", "Sport Teacher"),
+    )
+    speciality = models.CharField(max_length=50, choices=TITLE)
 
     def __str__(self):
         return self.name
